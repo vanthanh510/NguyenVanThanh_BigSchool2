d0988dc70aa6f0827a90e042cc23c1250fc40404
Administrator <Administrator@PC008>
2019-04-03 16:25:38 +0700

    Add project files.

---
+++ BigSchool2/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using BigSchool2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace BigSchool2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            var upcommingCourse = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+            return View(upcommingCourse);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
